8d4de46d4bc16e2f04e738adaf6be180dc2d3fa3
manzanaresDev <marcosmanzanaresdev@gmail.com>
2026-05-31 22:17:04 +0200

    message de validation

--- lib/contactSchema.ts
+++ lib/contactSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const contactSchema = z.object({
   name: z.string().min(2, "Nom trop court (min 2 caractères)!"),
-  email: z.string().email("Email invalide ' (caractère nom@server.dom)!"),
+  email: z.string().email("Email invalide ' (format nom@server.dom)!"),
   message: z.string().min(10, "Message trop court  (min 10 caractères)!"),
 });
 
