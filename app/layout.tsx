895c52bad17cf10aa663cdc5e2a1729d4529d841
manzanaresDev <marcosmanzanaresdev@gmail.com>
2026-06-22 21:49:36 +0200

    ajout: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Portfolio — Digital Experiences",
   description: "Full-stack developer & product designer portfolio",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const G_id = process.env.NEXT_PUBLIC_GA_ID;
   return (
     <html lang="fr">
       <body>{children}</body>
+      <GoogleAnalytics gaId={G_id} />
     </html>
   );
 }
